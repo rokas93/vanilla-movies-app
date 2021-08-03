2776b0cc5d10ae0ab06104b39663ba5b88ffd127
Rokas <rokasgiedraitis@gmail.com>
2021-08-03 18:27:40 +0300

    Added quiz-4 and app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+let quizSlider = document.querySelector('.quiz_slider');
+let quizSliderInner = document.querySelector('.quiz_slider_inner');
+
+let pressed = false;
+let startx;
+let x;
+
+quizSlider.addEventListener('mousedown', (e)=>{
+    pressed = true;
+    startx = e.offsetX - quizSliderInner.offsetLeft;
+    quizSlider.style.cursor = 'grabbing';
+});
+
+quizSlider.addEventListener('mouseup', ()=>{
+    quizSlider.style.cursor = 'grab';
+});
+
+window.addEventListener('mouseup', ()=>{
+    pressed = false;
+});
+
+quizSlider.addEventListener('mousemove', (e)=>{
+    if(pressed === true) {
+        e.preventDefault();
+        x = e.offsetX;
+        quizSliderInner.style.left = `${x - startx}px`;
+        checkBoundary();
+    }
+    return;
+});
+
+function checkBoundary(){
+    let outer = quizSlider.getBoundingClientRect();
+    let inner = quizSliderInner.getBoundingClientRect();
+
+    if(parseInt(quizSliderInner.style.left) > 0) {
+        quizSliderInner.style.left = '0px';
+    } else if (inner.right < outer.right) {
+        quizSliderInner.style.left = `-${inner.width - outer.width}px`;
+        console.log(quizSliderInner.style.left);
+    }
+}
