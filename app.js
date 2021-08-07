d87b821dd8b8c7c8ef54b2c4c7ad51ade4b3940d
Rokas <rokasgiedraitis@gmail.com>
2021-08-07 20:43:10 +0300

    Updated 08/07 v2

--- app.js
+++ app.js
@@ -1,58 +1,124 @@
-// let quizSlider = document.querySelector('.quiz_slider');
-// let quizSliderInner = document.querySelector('.quiz_slider_inner');
-
-// let pressed = false;
-// let startx;
-// let x;
-
-// quizSlider.addEventListener('mousedown', (e)=>{
-//     pressed = true;
-//     startx = e.offsetX - quizSliderInner.offsetLeft;
-//     quizSlider.style.cursor = 'grabbing';
-// });
-
-// quizSlider.addEventListener('mouseup', ()=>{
-//     quizSlider.style.cursor = 'grab';
-// });
-
-// window.addEventListener('mouseup', ()=>{
-//     pressed = false;
-// });
-
-// quizSlider.addEventListener('mousemove', (e)=>{
-//     if(pressed === true) {
-//         e.preventDefault();
-//         x = e.offsetX;
-//         quizSliderInner.style.left = `${x - startx}px`;
-//         checkBoundary();
-//     }
-//     return;
-// });
-
-// function checkBoundary(){
-//     let outer = quizSlider.getBoundingClientRect();
-//     let inner = quizSliderInner.getBoundingClientRect();
-
-//     if(parseInt(quizSliderInner.style.left) > 0) {
-//         quizSliderInner.style.left = '0px';
-//         quizSliderInner.style.transform = `translate3d(15%, 0px, 0px)`;
-//     } else if (inner.right < outer.right) {
-//         quizSliderInner.style.left = `${outer.width - inner.width}px`;
-//         quizSliderInner.style.transform = `translate3d(0px, 0px, 0px)`;
-//         console.log(quizSliderInner.style.left);
-//         console.log(outer.width);
-//         console.log(inner.width);
-//     }
-// }
-
-let flkty = new Flickity('.carousel');
-
+// results slider
 let nextButton = document.querySelector('.slider__buttons-next');
-nextButton.addEventListener( 'click', function() {
-    flkty.next( true );
-});
-
 let previousButton = document.querySelector('.slider__buttons-previous');
-previousButton.addEventListener( 'click', function() {
-    flkty.previous( true );
-});
+if (previousButton && nextButton) {
+    let flkty = new Flickity('.carousel');
+
+    nextButton.addEventListener('click', function() {
+        flkty.next(true);
+    });
+
+    previousButton.addEventListener('click', function() {
+        flkty.previous(true);
+    });
+}
+
+// changing favicon dynamically
+let link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = './favicon.ico';
+
+// quiz form
+let startBtn = document.querySelector('.hero__button');
+if (startBtn) {
+    startBtn.addEventListener('click', function() {
+        document.location = './quiz.html';
+    });
+}
+
+let infoBtn = document.querySelector('.quiz__information-block-button');
+let infoBlock = document.querySelector('.quiz__information');
+let typeBlock = document.querySelector('.quiz__type');
+let typePb = document.querySelector('.quiz__type-bar-progress');
+if (infoBtn) {
+    infoBtn.addEventListener('click', function() {
+        infoBlock.style.display = 'none';
+        typeBlock.style.display = 'block';
+
+        typePb.animate([
+            {width: '25%'},
+            {width: '50%'}
+        ], 
+            {duration: 500}
+        );
+    });
+}
+
+let selectFilmBtn = document.querySelector('#select-film');
+let formBlock = document.querySelector('.quiz__form');
+let formPb = document.querySelector('.quiz__form-bar-progress');
+if (selectFilmBtn) {
+    selectFilmBtn.addEventListener('click', function() {
+        typeBlock.style.display = 'none';
+        formBlock.style.display = 'block';
+
+        formPb.animate([
+            {width: '50%'},
+            {width: '75%'}
+        ], 
+            {duration: 500}
+        );
+    });
+}
+
+let selectTvBtn = document.querySelector('#select-tv');
+if (selectTvBtn) {
+    selectTvBtn.addEventListener('click', function () {
+        typeBlock.style.display = 'none';
+        formBlock.style.display = 'block';
+
+        formPb.animate([
+            {width: '50%'},
+            {width: '75%'}
+        ], 
+            {duration: 500}
+        );
+    });
+}
+
+let selectGenreBtn = document.querySelector('.quiz__form-button');
+if (selectGenreBtn) {
+    selectGenreBtn.addEventListener('click', function () {
+        location.replace('./results.html');
+    });
+}
+
+// first (results) quiz page pb animation
+let infoPb = document.querySelector('.quiz__information-bar-progress');
+if (infoPb) {
+    infoPb.animate([
+        {width: '0%'},
+        {width: '25%'}
+    ], 
+        {duration: 500}
+    );
+}
+
+// last (info) quiz page pb animation
+let resPb = document.querySelector('.quiz__results-bar-progress');
+if (resPb) {
+    resPb.animate([
+        {width: '75%'},
+        {width: '100%'}
+    ], 
+        {duration: 500}
+    );
+}
+
+let finishBtn = document.querySelector('.slider__buttons-select-finish');
+if (finishBtn) {
+    finishBtn.addEventListener('click', function () {
+        location.replace('./index.html');
+    });
+}
+
+let restartBtn = document.querySelector('.slider__buttons-select-again');
+if (restartBtn) {
+    restartBtn.addEventListener('click', function () {
+        location.replace('./quiz.html');
+    });
+}
