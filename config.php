63aeb7acfe7c614af9a439afc0ccda0bed9aded8
Rokas <rokasgiedraitis@gmail.com>
2021-08-09 22:27:42 +0300

    Added img and php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$mysql_config = [
+    'host'     => 'localhost',
+    'db_name'  => 'movie.lt',
+    'user'     => 'root',
+    'password' => ''
+];
---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once 'config.php';
+
+try {
+    $db = new PDO("mysql:host=" . $mysql_config['host'] . ";dbname=" . $mysql_config['db_name'], $mysql_config['user'], $mysql_config['password']);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Can't connect: <br>";
+    echo $e->getMessage();
+}
+
+$query = $db->prepare("SELECT `Name` FROM `moviedb` WHERE `Type`='Film' AND `Genre`='Crime'");
+$query->execute();
+$results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+http_response_code(200);
+
+echo json_encode($results);
