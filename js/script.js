1a6f7aaa6df2c850bf044a1862a961f269ac9e9b
Rokas <rokasgiedraitis@gmail.com>
2021-08-11 21:29:30 +0300

    Updated 08/11

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+let selectedFilm = document.querySelector('#select-film');
+let selectedTv = document.querySelector('#select-tv');
+
+
+
+let actionGenre = document.querySelector('#gaction');
+let comedyGenre = document.querySelector('#gcomedy');
+let dramaGenre = document.querySelector('#gdrama');
+let fantasyGenre = document.querySelector('#gfantasy');
+let horrorGenre = document.querySelector('#ghorror');
+let mysteryGenre = document.querySelector('#gmystery');
+let romanceGenre = document.querySelector('#gromance');
+let thrillerGenre = document.querySelector('#gthriller');
+
+
+
+
+
+let formBtn = document.querySelector('.quiz__form-button');
